1ea825919686a704d00044c37f63635bbd69a8fe
Mega067L <77284535+mega067@users.noreply.github.com>
2025-11-26 22:43:18 +0000

    nwe

--- zip.py
+++ zip.py
@@ -6,9 +6,8 @@ def zip_dir(path, ziph):
     for root, dirs, files in os.walk(path):
         for file in files:
             ziph.write(os.path.join(root, file),
-                       os.path.relpath(os.path.join(root, file),
-                                       os.path.join(path, '..')))
+                       os.path.relpath(os.path.join(root, file), path))
 
 if __name__ == '__main__':
-    with zipfile.ZipFile('OOSCAM_test.zip', 'w', zipfile.ZIP_DEFLATED) as zipf:
+    with zipfile.ZipFile('OOSCAM.zip', 'w', zipfile.ZIP_DEFLATED) as zipf:
         zip_dir('OOSCAM', zipf)
