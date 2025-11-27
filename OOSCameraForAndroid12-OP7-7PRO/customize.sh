646d4312760ee5eb30a37a1e3445ef3397d2aec1
Mega067L <77284535+mega067@users.noreply.github.com>
2025-11-27 15:46:56 +0000

    new files

--- OOSCameraForAndroid12-OP7-7PRO/customize.sh
+++ OOSCameraForAndroid12-OP7-7PRO/customize.sh
@@ -1,30 +1,30 @@
 ##########################################################################################
 #
-# Script de Configuración Extendido de MMT
+# MMT Extended Config Script
 #
 ##########################################################################################
 
 ##########################################################################################
-# Banderas de Configuración
+# Config Flags
 ##########################################################################################
 
-# Descomenta y cambia 'MINAPI' y 'MAXAPI' a la versión mínima y máxima de Android para tu mod
-# Descomenta DYNLIB si quieres que las librerías se instalen en vendor para Oreo+ y en system para versiones anteriores
-# Descomenta DEBUG si quieres registros de depuración completos (guardados en /sdcard)
+# Uncomment and change 'MINAPI' and 'MAXAPI' to the minimum and maximum android version for your mod
+# Uncomment DYNLIB if you want libs installed to vendor for oreo+ and system for anything older
+# Uncomment DEBUG if you want full debug logs (saved to /sdcard)
 MINAPI=31
 MAXAPI=35
 #DYNLIB=true
 #DEBUG=true
 
 ##########################################################################################
-# Lista de Reemplazo
+# Replace list
 ##########################################################################################
 
-# Lista todos los directorios que quieres reemplazar directamente en el sistema
-# Consulta la documentación para más información sobre por qué necesitarías esto
+# List all directories you want to directly replace in the system
+# Check the documentations for more info why you would need this
 
-# Construye tu lista en el siguiente formato
-# Este es un ejemplo
+# Construct your list in the following format
+# This is an example
 REPLACE_EXAMPLE="
 /system/app/Youtube
 /system/priv-app/SystemUI
@@ -32,37 +32,37 @@ REPLACE_EXAMPLE="
 /system/framework
 "
 
-# Construye tu propia lista aquí
+# Construct your own list here
 REPLACE="
 /system/product/app/Camera
 "
 
 ##########################################################################################
-# Permisos
+# Permissions
 ##########################################################################################
 
 set_permissions() {
-  : # Elimina esto si añades algo a esta función
+  : # Remove this if adding to this function
 
-  # Ten en cuenta que todos los archivos/carpetas en el directorio del módulo de Magisk tienen el prefijo $MODPATH - mantén este prefijo en todos tus archivos/carpetas
-  # Algunos ejemplos:
-
-  # Para directorios (incluye los archivos dentro de ellos):
-  # set_perm_recursive  <dirname>                <owner> <group> <dirpermission> <filepermission> <contexts> (por defecto: u:object_r:system_file:s0)
-
+  # Note that all files/folders in magisk module directory have the $MODPATH prefix - keep this prefix on all of your files/folders
+  # Some examples:
+  
+  # For directories (includes files in them):
+  # set_perm_recursive  <dirname>                <owner> <group> <dirpermission> <filepermission> <contexts> (default: u:object_r:system_file:s0)
+  
   # set_perm_recursive $MODPATH/system/lib 0 0 0755 0644
   # set_perm_recursive $MODPATH/system/vendor/lib/soundfx 0 0 0755 0644
 
-  # Para archivos (que no estén en directorios ya gestionados)
-  # set_perm  <filename>                         <owner> <group> <permission> <contexts> (por defecto: u:object_r:system_file:s0)
-
+  # For files (not in directories taken care of above)
+  # set_perm  <filename>                         <owner> <group> <permission> <contexts> (default: u:object_r:system_file:s0)
+  
   # set_perm $MODPATH/system/lib/libart.so 0 0 0644
   # set_perm /data/local/tmp/file.txt 0 0 644
 # set_perm /system/system_ext/priv-app/WallpaperPickerGoogleRelease/WallpaperPickerGoogleRelease.apk 0 0 644
 }
 
 ##########################################################################################
-# Lógica Extendida de MMT - No modifiques nada después de esto
+# MMT Extended Logic - Don't modify anything after this
 ##########################################################################################
 
 SKIPUNZIP=1
--- unzip.py
+++
@@ -1,4 +0,0 @@
-
-import zipfile
-with zipfile.ZipFile('OOSCameraForAndroid12-OP7-7PRO.zip', 'r') as zip_ref:
-    zip_ref.extractall('.')
