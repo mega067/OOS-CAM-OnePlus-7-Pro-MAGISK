f8f7326e47fa916136ac01aefa80219b00a8f00f
Mega067L <77284535+mega067@users.noreply.github.com>
2025-11-27 16:58:53 +0000

    #MAXAPI=35

--- OOSCameraForAndroid15-OP7-7PRO/customize.sh
+++ OOSCameraForAndroid15-OP7-7PRO/customize.sh
@@ -12,7 +12,7 @@
 # Uncomment DYNLIB if you want libs installed to vendor for oreo+ and system for anything older
 # Uncomment DEBUG if you want full debug logs (saved to /sdcard)
 MINAPI=31
-MAXAPI=35
+#MAXAPI=35
 #DYNLIB=true
 #DEBUG=true
 
