409905025f9485de367acb90d25ad95843730ae1
mega067 <angeljuares067@gmail.com>
2026-03-16 13:38:15 -0600

    Update OOS-CAM to v7.2.0-alpha: Ultra-Clean Vendor build

--- OOSCameraMODULE/customize.sh
+++ OOSCameraMODULE/customize.sh
@@ -1,40 +1,9 @@
 ##########################################################################################
-#
-# MMT Extended Config Script
-#
-##########################################################################################
-
-##########################################################################################
-# Config Flags
-##########################################################################################
-
-# Uncomment and change 'MINAPI' and 'MAXAPI' to the minimum and maximum android version for your mod
-# Uncomment DYNLIB if you want libs installed to vendor for oreo+ and system for anything older
-# Uncomment DEBUG if you want full debug logs (saved to /sdcard)
-MINAPI=31
-#MAXAPI=35
-#DYNLIB=true
-#DEBUG=true
-
-##########################################################################################
 # Replace list
 ##########################################################################################
 
-# List all directories you want to directly replace in the system
-# Check the documentations for more info why you would need this
-
-# Construct your list in the following format
-# This is an example
-REPLACE_EXAMPLE="
-/system/app/Youtube
-/system/priv-app/SystemUI
-/system/priv-app/Settings
-/system/framework
-"
-
-# Construct your own list here
+# ¡DEJAR VACÍO PARA EVITAR BOOTLOOPS!
 REPLACE="
-/vendor/etc
 "
 
 ##########################################################################################
@@ -43,72 +12,62 @@ REPLACE="
 
 set_permissions() {
   ui_print "*********************************************"
-  ui_print "*                                           *"
-  ui_print "*    ██████╗ ██████╗  ██████╗ █████╗ ███╗   ███╗ *"
-  ui_print "*   ██╔═══██╗██╔══██╗██╔════╝██╔══██╗████╗ ████║ *"
-  ui_print "*   ██║   ██║██████╔╝██║     ███████║██╔████╔██║ *"
-  ui_print "*   ██║   ██║██╔═══╝ ██║     ██╔══██║██║╚██╔╝██║ *"
-  ui_print "*   ╚██████╔╝██║     ╚██████╗██║  ██║██║ ╚═╝ ██║ *"
-  ui_print "*    ╚═════╝ ╚═╝      ╚═════╝╚═╝  ╚═╝╚═╝     ╚═╝ *"
-  ui_print "*                                           *"
-  ui_print "*    OnePlus Camera Port - v6.5.3 (Alpha)   *"
+  ui_print "* *"
+  ui_print "* ██████╗ ██████╗  ██████╗ █████╗ ███╗   ███╗ *"
+  ui_print "* ██╔═══██╗██╔══██╗██╔════╝██╔══██╗████╗ ████║ *"
+  ui_print "* ██║   ██║██████╔╝██║     ███████║██╔████╔██║ *"
+  ui_print "* ██║   ██║██╔═══╝ ██║     ██╔══██║██║╚██╔╝██║ *"
+  ui_print "* ╚██████╔╝██║     ╚██████╗██║  ██║██║ ╚═╝ ██║ *"
+  ui_print "* ╚═════╝ ╚═╝      ╚═════╝╚═╝  ╚═╝╚═╝     ╚═╝ *"
+  ui_print "* *"
+  ui_print "*    OnePlus Camera Port - v7.2.0 (Alpha)   *"
   ui_print "*********************************************"
-  ui_print "- Device: OnePlus 7 / 7 Pro / 7T / 7T Pro"
-  ui_print "- Android: 14 / 15 / 16 (AOSP)"
-  ui_print "- Developer: Mega067 & Antigravity"
+  ui_print "- Device: OnePlus 7 / 7 Pro (guacamole)"
+  ui_print "- Version: 7.2 - Android 16 Compatible"
+  ui_print "- Developer: Angel & Antigravity"
   ui_print "*********************************************"
 
   ui_print "[#] Setting permissions..."
   
-  # Set permissions for system files
+  # Permisos para el sistema y las librerías vendor corregidas
   set_perm_recursive $MODPATH/system 0 0 0755 0644
   
-  # Set permissions for the APK specifically
-  set_perm $MODPATH/system/priv-app/OnePlusCamera/OnePlusCamera_FIX_v3.apk 0 0 0644
+  # Permisos específicos para los APKs (usando comodines por seguridad)
+  ui_print "[#] Optimizing APK permissions..."
+  set_perm $MODPATH/system/priv-app/OnePlusCamera/OnePlusCamera.apk 0 0 0644
+  set_perm $MODPATH/system/priv-app/OnePlusCameraService/OnePlusCameraService.apk 0 0 0644
+  set_perm $MODPATH/system/priv-app/OnePlusGallery/OnePlusGallery.apk 0 0 0644
 
-  # Set permissions for binary/libs if any
-  if [ -d "$MODPATH/system/priv-app/OnePlusCamera/lib" ]; then
-    ui_print "[#] Setting library permissions..."
-    set_perm_recursive $MODPATH/system/priv-app/OnePlusCamera/lib 0 0 0755 0644
-  fi
-
-  # Set permissions for scripts
+  # Permisos para scripts de soporte
   set_perm $MODPATH/action.sh 0 0 0755
+  [ -f "$MODPATH/post-fs-data.sh" ] && set_perm $MODPATH/post-fs-data.sh 0 0 0755
 
-  # Set permissions for sepolicy
+  # Aplicar reglas de SELinux para que la cámara acceda al hardware en A16
   ui_print "[#] Applying SEPolicy rules..."
-  set_perm_recursive $MODPATH/sepolicy u:object_r:system_file:s0 0 0 0755 0644
+  if [ -d "$MODPATH/sepolicy" ]; then
+    set_perm_recursive $MODPATH/sepolicy u:object_r:system_file:s0 0 0 0755 0644
+  fi
 
   ui_print "---------------------------------------------"
   
   if [ "$KSU" = "true" ]; then
     ui_print "[!] KernelSU detected!"
-    ui_print "- Trying to auto-install APK for KernelSU..."
-    # Attempt to install the app via pm
-    if [ -f "$MODPATH/system/priv-app/OnePlusCamera/OnePlusCamera_FIX_v3.apk" ]; then
-       # pm install -r might not work in all recovery environments, but works in KSU Manager
-       pm install -r "$MODPATH/system/priv-app/OnePlusCamera/OnePlusCamera_FIX_v3.apk" >/dev/null 2>&1
-       ui_print "- Installation command sent to PM."
-    fi
-    ui_print "- NOTE: If the app doesn't appear after reboot,"
-    ui_print "  please install the APK manually from:"
-    ui_print "  /data/adb/modules/OnePlusCamera/system/priv-app/..."
+    ui_print "- Installing APKs via Package Manager..."
+    
+    # Instalación silenciosa para que aparezcan en el cajón de apps de inmediato
+    pm install -r "$MODPATH/system/priv-app/OnePlusCamera/OnePlusCamera.apk" >/dev/null 2>&1
+    pm install -r "$MODPATH/system/priv-app/OnePlusCameraService/OnePlusCameraService.apk" >/dev/null 2>&1
+    pm install -r "$MODPATH/system/priv-app/OnePlusGallery/OnePlusGallery.apk" >/dev/null 2>&1
+    
+    ui_print "- Installation commands sent."
   else
     ui_print "[*] Magisk detected!"
   fi
 
   ui_print "*********************************************"
-  ui_print "*        INSTALLATION SUCCESSFUL!           *"
+  ui_print "* INSTALLATION SUCCESSFUL!           *"
   ui_print "*********************************************"
   ui_print "- Please reboot your device."
-  ui_print "- Clear Camera cache/data after reboot."
+  ui_print "- IMPORTANT: Clear Camera data after reboot."
   ui_print "*********************************************"
 }
-
-##########################################################################################
-# MMT Extended Logic - Don't modify anything after this
-##########################################################################################
-
-SKIPUNZIP=1
-unzip -qjo "$ZIPFILE" 'common/functions.sh' -d $TMPDIR >&2
-. $TMPDIR/functions.sh
