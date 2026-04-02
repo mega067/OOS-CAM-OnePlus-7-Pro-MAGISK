45434db86d2bda862988835dec894a4924e61db6
mega067 <angeljuares067@gmail.com>
2026-04-02 13:47:12 -0600

    chore: deep clean repo and ignore artifacts

---
+++ OOSCameraMODULE/action.sh
@@ -0,0 +1,4 @@
+#!/system/bin/sh
+
+# Launch the camera activity
+am start -n com.oneplus.camera/com.oneplus.camera.CameraActivity
---
+++ OOSCameraMODULE/post-fs-data.sh
@@ -0,0 +1,15 @@
+#!/system/bin/sh
+# Este script se ejecuta en el espacio de nombres de post-fs-data
+
+MODDIR=${0%/*}
+
+
+# Fix for Watermark - Create necessary directory
+if [ ! -d "/data/vendor/camera/watermark" ]; then
+    mkdir -p /data/vendor/camera/watermark
+fi
+
+# Set permissions and ownership
+chmod 0777 /data/vendor/camera/watermark
+chown -R camera:camera /data/vendor/camera/watermark
+restorecon -R /data/vendor/camera/watermark
